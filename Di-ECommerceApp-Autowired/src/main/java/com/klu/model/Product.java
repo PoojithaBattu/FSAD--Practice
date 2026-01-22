0e9f1d3a2f9cb2bae59923d64b5b61f8a74b60e6
PoojithaBattu <2400030910@kluniversity.in>
2026-01-22 12:47:09 +0530

    autowired-22/1/26

---
+++ Di-ECommerceApp-Autowired/src/main/java/com/klu/model/Product.java
@@ -0,0 +1,35 @@
+package com.klu.model;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Product {
+
+	private int productId;
+	private String productName;
+	private double price;
+	private String category;
+
+	public Product() {
+		this.productId = 100;
+		this.productName = "Laptop";
+		this.price = 65000.00;
+		this.category = "Electronic";
+	}
+
+	public int getProductId() {
+		return productId;
+	}
+
+	public String getProductName() {
+		return productName;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public String getCategory() {
+		return category;
+	}
+}
