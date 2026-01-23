920fa209d1d67dcf0ad27c24cd08c8508783d36d
PoojithaBattu <2400030910@kluniversity.in>
2026-01-23 07:35:11 +0530

    19/12/25

---
+++ hubernate-create/src/main/java/com/klu/MainApp.java
@@ -0,0 +1,30 @@
+package com.klu;
+
+import org.hibernate.*;
+
+import org.hibernate.cfg.Configuration;
+public class MainApp {
+	public static void main(String[] args) {
+
+	Configuration cfg = new Configuration();
+    cfg.configure("hibernate.cfg.xml");
+    cfg.addAnnotatedClass(Student.class);
+
+    SessionFactory factory = cfg.buildSessionFactory();
+    Session session = factory.openSession();
+
+    Transaction tx = session.beginTransaction();
+
+    Student s = new Student("Pooja");
+    session.save(s);
+
+    tx.commit();
+
+    session.close();
+    factory.close();
+
+    System.out.println("Student data saved successfully");
+
+	}
+
+}
