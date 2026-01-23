0729c6a87f17001d9a3e232fc731eea3dba670ba
PoojithaBattu <2400030910@kluniversity.in>
2026-01-23 07:36:45 +0530

    17/12/25

---
+++ jdbc-create/src/main/java/com/klu/JDBCcreate.java
@@ -0,0 +1,23 @@
+package com.klu;
+import java.sql.*;
+public class JDBCcreate {
+	public static void main(String[] arg) {
+		String url="jdbc:mysql://localhost:3306/fsaddb";
+		String usr="root";
+		String pwd="123456";
+		try {
+			Connection con=DriverManager.getConnection(url,usr,pwd);
+			System.out.println("Connection established");
+			String query="create table if not exists Student ("+ "id int primary key auto_increment," + "name varchar(20)"+")";
+			Statement st=con.createStatement();
+			st.execute(query);
+			System.out.println("Table Created");
+			con.close();
+
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+}
