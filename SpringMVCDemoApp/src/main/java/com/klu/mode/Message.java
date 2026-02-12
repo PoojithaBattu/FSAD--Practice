59a3f36f6ea14318aa130ce410e809e6512f967b
PoojithaBattu <2400030910@kluniversity.in>
2026-02-12 09:34:50 +0530

    MVC-11/02/26

---
+++ SpringMVCDemoApp/src/main/java/com/klu/mode/Message.java
@@ -0,0 +1,13 @@
+package com.klu.mode;
+
+public class Message {
+	private String text;
+	public Message(String text) {
+		this.text = text;
+	}
+	public String getText() {
+		return text;
+	}
+	
+
+}
