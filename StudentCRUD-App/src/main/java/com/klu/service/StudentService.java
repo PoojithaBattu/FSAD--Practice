e0f659ae02fde5d16ade2ad44ea6bf613c25533f
PoojithaBattu <2400030910@kluniversity.in>
2026-02-19 09:33:33 +0530

    18/02/2026

---
+++ StudentCRUD-App/src/main/java/com/klu/service/StudentService.java
@@ -0,0 +1,16 @@
+package com.klu.service;
+
+import java.util.List;
+
+import com.klu.model.Student;
+
+public interface StudentService {
+	  String getWelcomeMessage();
+	    Student getStudentById(int id);
+	    Student createStudent(Student student);
+	    List<Student> getAllStudents();
+	    Student updateStudent(int id, Student student);
+	    String deleteStudent(int id);
+	    List<Student> searchStudent(String name, String course);
+
+}
